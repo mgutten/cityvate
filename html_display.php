364506b0f517f1eb77f2d71a63ed8010db1d7673
Marshall <marf7001@aol.com>
2012-06-11 17:34:25 -0700

    working to get arrows working with ajax call on member homepage

--- html_display.php
+++ html_display.php
@@ -315,7 +315,7 @@ class Body_member extends Body {
 			parent::__construct();
 	}
 	
-					
+						
 }
 	
 
@@ -591,6 +591,71 @@ function signup_date_options($num_months) {
 		}
 }
 
+//member homepage top section html
+function activity_home_top($selected_month = '') {
+			//make call to retrieve activities list
+			$activities_call = new Activities();
+			$activities = $activities_call->activities($selected_month);
+		
+			$block = "<a href='activity.php?num=".$activities[0]['aID']."' id='picture_link'>
+						<div id='picture'>
+							<div id='picture_shown_outer'>
+								<img src='../images/activities/".str_replace(' ','_',$activities[0]['name']).".jpg' class='activity_picture shown' id='picture_shown'/>
+							 </div>
+							 <div id='picture_hidden_outer'>
+								<img src='' id='picture_hidden'/>
+							 </div>
+				
+								
+									<div id='picture_top'>
+									<div class='picture_banner' id='picture_top_banner'>
+									</div>
+									<p class='text banner' id='picture_banner_text'>
+											".$activities[0]['name']."
+									</p>
+									<div class='picture_banner' id='picture_bottom_banner'>
+									</div>
+									<p class='text banner' id='click_banner'>
+											Click for details
+									</p>
+								</div>
+						</div>
+						</a>
+						
+						<div id='top_right_month'>
+							<div id='left_arrow' class='arrow'></div>";
+			if($selected_month < date('m') && $selected_month != ''){
+				$block .= "<div id='right_arrow' class='arrow'></div>";
+				//convert month number to month name
+				$month_name = date( 'F', mktime(0, 0, 0, $selected_month) );
+				$block .= "<p id='".$selected_month.date('F')."' class='text activity_month'>".$month_name."'s Activities</p>";
+			}
+			else{
+				$block .= "<p id='".date('n').date('F')."' class='text activity_month'>".date('F')."'s Activities</p>";
+			}
+				$block .= "</div>
+						<div id='top_right_activities'>";
+						
+								//populate with list of activities from array
+								for($i=0;$i<count($activities);$i++){
+										$class = 'activity text';
+										//if no reserve date set, echo link to calendar------------------------------------------------
+										if(empty($activities[$i]['reserve_date']))
+											$reserve_date = '<img src="../images/member/plus.png" title="Add to Calendar" class="activity_reserve plus"/>';
+										else{
+											$date = DateTime::createFromFormat('Y-m-d', $activities[$i]['reserve_date']);
+											$reserve_date = "<p class='activity_reserve'>".$date->format('M j')."</p>";
+										}
+										if($i==0)
+											$class .= ' selected';
+									$block .= "<div class='$class' id='".$activities[$i]['aID']."'><p class='activity_name'>".$activities[$i]['name']."</p><p class='activity_type'>".$activities[$i]['type']."</p><a href='calendar.php'>$reserve_date</a></div>";
+									
+								}
+					
+				$block .= "</div>";
+				
+				echo $block;
+	}
 	
 		
 			
